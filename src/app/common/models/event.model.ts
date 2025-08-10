275b16e1f82bd731a956077f1a282e002f2f70f5
hrafiee1995@gmail.com <hrafiee1995@gmail.com>
2025-08-10 10:31:08 +0330

    feat(models): add organizer and isPublic fields to EventModel

--- src/app/common/models/event.model.ts
+++ src/app/common/models/event.model.ts
@@ -2,10 +2,12 @@ export interface EventModel {
   id?: string;
   title: string;
   organizerId?: string;
+  organizer?: { id: string; businessName?: string };
   venueId: string;
   startDateTime: string;
   endDateTime: string;
   timezone: string;
+  isPublic?: boolean;
   description?: string;
   primaryImageUrl?: string;
   coverImageUrl?: string;
