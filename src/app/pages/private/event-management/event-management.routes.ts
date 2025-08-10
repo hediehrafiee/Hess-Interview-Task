7f0d5b33c92e25dd4c77972e29f7d1b70da29f8d
hrafiee1995@gmail.com <hrafiee1995@gmail.com>
2025-08-10 16:00:03 +0330

    chore: event other routes

--- src/app/pages/private/event-management/event-management.routes.ts
+++ src/app/pages/private/event-management/event-management.routes.ts
@@ -7,4 +7,14 @@ export const EVENT_MANAGEMENT_ROUTES: Routes = [
     loadComponent: async () =>
       await import('./event-list/event-list.component'),
   },
+  {
+    path: 'new',
+    loadComponent: async () =>
+      await import('./event-form/event-form.component'),
+  },
+  {
+    path: ':id/edit',
+    loadComponent: async () =>
+      await import('./event-form/event-form.component'),
+  },
 ];
