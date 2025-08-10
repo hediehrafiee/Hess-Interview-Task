bf0b6aca5068633cfbc9922316acf659923a0c7f
hrafiee1995@gmail.com <hrafiee1995@gmail.com>
2025-08-10 10:39:15 +0330

    chore: change login path route navigation

--- src/app/pages/public/login/login.component.ts
+++ src/app/pages/public/login/login.component.ts
@@ -1,26 +1,39 @@
-import {Component, OnDestroy, OnInit, signal} from '@angular/core';
-import {Router} from '@angular/router';
-import {AuthService} from '../../../core/services/auth.service';
-import {LoginFormModel, LoginModel} from '../../../common/models/dtos/login.model';
-import {NoticeService} from '../../../common/services/notice.service';
-import {HaPanelComponent} from '../../../common/components/ha-panel/ha-panel.component';
-import {FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {RxFormBuilder, RxReactiveFormsModule} from '@rxweb/reactive-form-validators';
-import {Subject, takeUntil} from 'rxjs';
-import {SharedModule} from '../../../common/shared.module';
-import {NzFormModule} from 'ng-zorro-antd/form';
-import {NzInputModule} from 'ng-zorro-antd/input';
-import {NzButtonModule} from 'ng-zorro-antd/button';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import {
+  LoginFormModel,
+  LoginModel,
+} from '../../../common/models/dtos/login.model';
+import { NoticeService } from '../../../common/services/notice.service';
+import { HaPanelComponent } from '../../../common/components/ha-panel/ha-panel.component';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  RxFormBuilder,
+  RxReactiveFormsModule,
+} from '@rxweb/reactive-form-validators';
+import { Subject, takeUntil } from 'rxjs';
+import { SharedModule } from '../../../common/shared.module';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-login',
-  imports: [SharedModule, HaPanelComponent, FormsModule, ReactiveFormsModule, RxReactiveFormsModule, NzFormModule, NzInputModule,
-  NzButtonModule],
+  imports: [
+    SharedModule,
+    HaPanelComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    RxReactiveFormsModule,
+    NzFormModule,
+    NzInputModule,
+    NzButtonModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent implements OnInit, OnDestroy {
-
   // States
   readonly passwordVisible = signal(false);
   readonly isSubmitting = signal(false);
@@ -35,9 +48,8 @@ export class LoginComponent implements OnInit, OnDestroy {
     private _auth: AuthService,
     private _router: Router,
     private _notice: NoticeService,
-    private _rxFormBuilder: RxFormBuilder,
-  ) {
-  }
+    private _rxFormBuilder: RxFormBuilder
+  ) {}
 
   ngOnInit() {
     this.formGroup = this._rxFormBuilder.formGroup(new LoginFormModel());
@@ -54,11 +66,12 @@ export class LoginComponent implements OnInit, OnDestroy {
     this.isSubmitting.set(true);
 
     const data: LoginModel = this.formGroup.value;
-    this._auth.login(data)
+    this._auth
+      .login(data)
       .pipe(takeUntil(this.destroy$))
       .subscribe({
         next: () => {
-          this._router.navigate(["/p/dashboard"]);
+          this._router.navigate(['/p']);
         },
         complete: () => {
           this.isSubmitting.set(false);
@@ -72,10 +85,13 @@ export class LoginComponent implements OnInit, OnDestroy {
       this._notice.showWarningMessage('Email is required');
       return;
     }
-    this._auth.forgotPassword(email)
+    this._auth
+      .forgotPassword(email)
       .pipe(takeUntil(this.destroy$))
       .subscribe(() => {
-        this._notice.showSuccessMessage('Password reset link sent to your email inbox.');
+        this._notice.showSuccessMessage(
+          'Password reset link sent to your email inbox.'
+        );
       });
   }
 
